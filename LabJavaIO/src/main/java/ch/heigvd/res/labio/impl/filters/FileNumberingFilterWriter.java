fe73d8a11a01a42c9fff38cc8592c7caeaee25e8
Claude-André <claudeuw@hotmail.com>
2019-03-13 16:39:34 +0100

    passage des tests de CompleteFileTransformer.java

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -10,66 +10,66 @@ import java.util.logging.Logger;
  * When filter encounters a line separator, it sends it to the decorated writer.
  * It then sends the line number and a tab character, before resuming the write
  * process.
- *
+ * <p>
  * Hello\n\World -> 1\tHello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
-  private boolean start = true;
-  private int counter = 1;
-  private boolean cloche = false;
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+    private boolean start = true;
+    private int counter = 1;
+    private boolean cloche = false;
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    for (int i = off; i < off + len; i++) {
-      write(str.charAt(i));
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
     }
 
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        for (int i = off; i < off + len; i++) {
+            write(str.charAt(i));
+        }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    for (int i = off; i < off + len; i++) {
-      write(cbuf[i]);
-    }
-  }
-
-  @Override
-  public void write(int c) throws IOException {
-
-    if(start) {
-      super.write(counter++ + "\t", 0, 2);
-      start = false;
     }
 
-
-    if (cloche) {
-      if (c != '\r') {
-        cloche = false;
-      }
-      if (c == '\n') {
-        super.write("\r\n" + counter + "\t", 0, 3 + String.valueOf(counter++).length());
-
-      } else {
-        super.write( "\r" + counter + "\t" + (char) c, 0, 3 + String.valueOf(counter++).length());
-      }
-    } else {
-      if (c == '\r') {
-        cloche = true;
-      } else if (c == '\n') {
-        super.write("\n" + counter + "\t", 0, 2 + String.valueOf(counter++).length());
-      } else {
-        super.write(c);
-      }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        for (int i = off; i < off + len; i++) {
+            write(cbuf[i]);
+        }
     }
 
-  }
+    @Override
+    public void write(int c) throws IOException {
+
+        if (start) {
+            super.write(counter++ + "\t", 0, 2);
+            start = false;
+        }
+
+        if (cloche) {
+            if (c != '\r') {
+                cloche = false;
+            }
+            if (c == '\n') {
+                super.write("\r\n" + counter + "\t", 0, 3 + String.valueOf(counter++).length());
+
+            } else {
+                super.write("\r" + counter + "\t" + (char) c, 0, 3 + String.valueOf(counter++).length());
+            }
+        } else {
+            if (c == '\r') {
+                cloche = true;
+            } else if (c == '\n') {
+                super.write("\n" + counter + "\t", 0, 2 + String.valueOf(counter++).length());
+            } else {
+                super.write(c);
+            }
+        }
+
+    }
 
 }
