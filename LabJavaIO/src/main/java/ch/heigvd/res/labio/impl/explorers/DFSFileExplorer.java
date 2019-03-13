51f3b1f2c3c2aa804ae1ea93449392c765c15008
Claude-André <claudeuw@hotmail.com>
2019-03-13 17:27:11 +0100

    mise en page pour fin labo

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -2,6 +2,7 @@ package ch.heigvd.res.labio.impl.explorers;
 
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
+
 import java.io.File;
 import java.util.Arrays;
 
@@ -10,21 +11,21 @@ import java.util.Arrays;
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor visitor) {
-    visitor.visit(rootDirectory);
-    if(rootDirectory.isDirectory()) {
-      File[] rootChildren = rootDirectory.listFiles();
-      Arrays.sort(rootChildren);
-      for ( File f : rootChildren) {
-        explore(f, visitor);
-      }
+    @Override
+    public void explore(File rootDirectory, IFileVisitor visitor) {
+        visitor.visit(rootDirectory);
+        if (rootDirectory.isDirectory()) {
+            File[] rootChildren = rootDirectory.listFiles();
+            Arrays.sort(rootChildren);
+            for (File f : rootChildren) {
+                explore(f, visitor);
+            }
+        }
     }
-  }
 
 }
--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -5,33 +5,32 @@ import java.io.IOException;
 import java.io.Writer;
 
 /**
- *
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    super.write(str.toUpperCase(), off, len);
-  }
-
-
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    char[] cbufUpper = new char[len];
-    for(int i = 0; i < len; i++) {
-      cbufUpper[i] = Character.toUpperCase(cbuf[off + i]);
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
     }
-    super.write(cbufUpper, 0, len);
-  }
 
-  @Override
-  public void write(int c) throws IOException {
-    super.write(Character.toUpperCase(c));
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        super.write(str.toUpperCase(), off, len);
+    }
+
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        char[] cbufUpper = new char[len];
+        for (int i = 0; i < len; i++) {
+            cbufUpper[i] = Character.toUpperCase(cbuf[off + i]);
+        }
+        super.write(cbufUpper, 0, len);
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        super.write(Character.toUpperCase(c));
+    }
 
 }
